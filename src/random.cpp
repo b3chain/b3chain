b3chain/b3chain

commit 61a60e3985dca45e22c0e2e3b1bb3ce5bd0260d7

break circular dependency: random/sync -> util -> random/sync

src/random.cpp
@@ -11,14 +11,15 @@
 #include <compat.h> // for Windows API
 #include <wincrypt.h>
 #endif
-#include <util.h>             // for LogPrint()
-#include <utilstrencodings.h> // for GetTime()
+#include <logging.h>  // for LogPrint()
+#include <utiltime.h> // for GetTime()
 
 #include <stdlib.h>
 #include <chrono>
 #include <thread>
 
 #ifndef WIN32
+#include <fcntl.h>
 #include <sys/time.h>
 #endif
 

src/sync.cpp
@@ -4,12 +4,14 @@
 
 #include <sync.h>
 
-#include <memory>
-#include <set>
-#include <util.h>
+#include <logging.h>
 #include <utilstrencodings.h>
 
 #include <stdio.h>
+
+#include <map>
+#include <memory>
+#include <set>
 
 #ifdef DEBUG_LOCKCONTENTION
 #if !defined(HAVE_THREAD_LOCAL)

commit 5bbc92dab976736b6e604d57913aa191b9bd1a91

Fix FreeBSD build by including utilstrencodings.h
`random.cpp` needs to explicitly include `utilstrencodings.h` to get
`ARRAYLEN`. This fixes the FreeBSD build.

This was broken in 61a60e3985dca45e22c0e2e3b1bb3ce5bd0260d7.

src/random.cpp
@@ -34,6 +34,7 @@
 #include <sys/random.h>
 #endif
 #ifdef HAVE_SYSCTL_ARND
+#include <utilstrencodings.h> // for ARRAYLEN
 #include <sys/sysctl.h>
 #endif
 