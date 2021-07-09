jonatack/bitcoin

net: change assumed I2P port to 0
* When accepting an I2P connection, assume the peer has port 0 instead
  of the default 8333 (for mainnet). It is not being sent to us, so we
  must assume something.
* When deriving our own I2P listen CService use port 0 instead of the
  default 8333 (for mainnet). So that we later advertise it to peers
  with port 0.

In the I2P protocol SAM 3.1 and older (we use 3.1) ports are not used,
so they are irrelevant. However in SAM 3.2 and newer ports are used and
from the point of view of SAM 3.2, a peer using SAM 3.1 seems to have
specified port=0.

src/i2p.cpp
@@ -159,7 +159,7 @@ bool Session::Accept(Connection& conn)
             const std::string& peer_dest =
                 conn.sock->RecvUntilTerminator('\n', MAX_WAIT_FOR_IO, *m_interrupt, MAX_MSG_SIZE);
 
-            conn.peer = CService(DestB64ToAddr(peer_dest), Params().GetDefaultPort());
+            conn.peer = CService(DestB64ToAddr(peer_dest), PORT_SAM31);
 
             return true;
         }
@@ -366,7 +366,7 @@ void Session::CreateIfNotCreatedAlready()
     SendRequestAndGetReply(*sock, strprintf("SESSION CREATE STYLE=STREAM ID=%s DESTINATION=%s",
                                             session_id, private_key_b64));
 
-    m_my_addr = CService(DestBinToAddr(MyDestination()), Params().GetDefaultPort());
+    m_my_addr = CService(DestBinToAddr(MyDestination()), PORT_SAM31);
     m_session_id = session_id;
     m_control_sock = std::move(sock);
 

b3chain/b3chain

src/i2p.cpp
@@ -159,7 +159,7 @@ bool Session::Accept(Connection& conn)
             const std::string& peer_dest =
                 conn.sock->RecvUntilTerminator('\n', MAX_WAIT_FOR_IO, *m_interrupt, MAX_MSG_SIZE);
 
-            conn.peer = CService(DestB64ToAddr(peer_dest), Params().GetDefaultPort());
+            conn.peer = CService(DestB64ToAddr(peer_dest), I2P_SAM31_PORT);
 
             return true;
         }
@@ -366,7 +366,7 @@ void Session::CreateIfNotCreatedAlready()
     SendRequestAndGetReply(*sock, strprintf("SESSION CREATE STYLE=STREAM ID=%s DESTINATION=%s",
                                             session_id, private_key_b64));
 
-    m_my_addr = CService(DestBinToAddr(MyDestination()), Params().GetDefaultPort());
+    m_my_addr = CService(DestBinToAddr(MyDestination()), I2P_SAM31_PORT);
     m_session_id = session_id;
     m_control_sock = std::move(sock);
 