surendrailla36/Bitcoin

[refactor] move valid tx processing to TxDownload

src/node/txdownloadman_impl.cpp
@@ -206,4 +206,16 @@ std::optional<PackageToValidate> TxDownloadImpl::Find1P1CPackage(const CTransact
     }
     return std::nullopt;
 }
+
+void TxDownloadImpl::MempoolAcceptedTx(const CTransactionRef& tx)
+{
+    // As this version of the transaction was acceptable, we can forget about any requests for it.
+    // No-op if the tx is not in txrequest.
+    m_txrequest.ForgetTxHash(tx->GetHash());
+    m_txrequest.ForgetTxHash(tx->GetWitnessHash());
+
+    m_orphanage.AddChildrenToWorkSet(*tx);
+    // If it came from the orphanage, remove it. No-op if the tx is not in txorphanage.
+    m_orphanage.EraseTx(tx->GetWitnessHash());
+}
 } // namespace node

b3chain/b3chain

src/node/txdownloadman_impl.cpp
@@ -75,6 +75,10 @@ std::optional<PackageToValidate> TxDownloadManager::Find1P1CPackage(const CTrans
 {
     return m_impl->Find1P1CPackage(ptx, nodeid);
 }
+void TxDownloadManager::MempoolAcceptedTx(const CTransactionRef& tx)
+{
+    m_impl->MempoolAcceptedTx(tx);
+}
 
 // TxDownloadManagerImpl
 void TxDownloadManagerImpl::ActiveTipChange()
@@ -271,5 +275,17 @@ std::optional<PackageToValidate> TxDownloadManagerImpl::Find1P1CPackage(const CT
         }
     }
     return std::nullopt;
+}
+
+void TxDownloadManagerImpl::MempoolAcceptedTx(const CTransactionRef& tx)
+{
+    // As this version of the transaction was acceptable, we can forget about any requests for it.
+    // No-op if the tx is not in txrequest.
+    m_txrequest.ForgetTxHash(tx->GetHash());
+    m_txrequest.ForgetTxHash(tx->GetWitnessHash());
+
+    m_orphanage.AddChildrenToWorkSet(*tx);
+    // If it came from the orphanage, remove it. No-op if the tx is not in txorphanage.
+    m_orphanage.EraseTx(tx->GetWitnessHash());
 }
 } // namespace node