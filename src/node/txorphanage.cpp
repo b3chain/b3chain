glozow/bitcoin

[cleanup] remove unused rng param from LimitOrphans

src/node/txorphanage.cpp
@@ -30,7 +30,7 @@ void TxOrphanage::EraseForPeer(NodeId peer)
     m_impl->EraseForPeer(peer);
 }
 
-void TxOrphanage::LimitOrphans(FastRandomContext& rng)
+void TxOrphanage::LimitOrphans()
 {
     m_impl->LimitOrphans();
 }

b3chain/b3chain

src/node/txorphanage.cpp
@@ -216,7 +216,7 @@ public:
     bool EraseTx(const Wtxid& wtxid) override;
     void EraseForPeer(NodeId peer) override;
     void EraseForBlock(const CBlock& block) override;
-    void LimitOrphans(FastRandomContext& rng) override;
+    void LimitOrphans() override;
     void AddChildrenToWorkSet(const CTransaction& tx, FastRandomContext& rng) override;
     bool HaveTxToReconsider(NodeId peer) override;
     std::vector<CTransactionRef> GetChildrenFromSamePeer(const CTransactionRef& parent, NodeId nodeid) const override;
@@ -408,7 +408,7 @@ void TxOrphanageImpl::EraseForPeer(NodeId peer)
  * amount of announcements and space for each peer. The reserved amount is protected from eviction even if there
  * are peers spamming the orphanage.
  */
-void TxOrphanageImpl::LimitOrphans(FastRandomContext& rng)
+void TxOrphanageImpl::LimitOrphans()
 {
     if (!NeedsTrim()) return;
 